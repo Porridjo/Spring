18d559649c9a38b453551ee9e5d2639df07d12a9
Porridjo <Den.nguyen69@gmail.com>
2023-10-18 12:25:49 +0200

    add authentication service

--- Cours 3/Wishlists/src/main/java/com/example/wishlists/WishlistController.java
+++ Cours 3/Wishlists/src/main/java/com/example/wishlists/WishlistController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ public class WishlistController {
   }
 
 
-  @PutMapping("/wishlists/{pseudo}/{productId}")
+  @PostMapping("/wishlists/{pseudo}/{productId}")
   public ResponseEntity<Void> updateOne(@PathVariable String pseudo, @PathVariable int productId,
       @RequestBody Wishlist wishlist) {
     if (!pseudo.equals(wishlist.getPseudo()) || productId != wishlist.getProductId()) {
